f7396e52bf0e5c3d251b1601e43b635856c51cc8
Maksym Kidanov <Eldiwr97@gmail.com>
2023-07-07 15:34:36 +0300

    API cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import { CustomFilter, Hero, SearchBar } from '@/components'
+import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
+import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+
+  const allCars = await fetchCars();
+  const isDadaEmpty = !Array.isArray(allCars) || allCars.length <1 || !allCars;
   return (
     <main className="overflow-hidden">
       <Hero /> 
@@ -20,6 +24,20 @@ export default function Home() {
             <CustomFilter title="fuel" /> */}
           </div>
         </div>
+
+        {!isDadaEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car)=>(
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ): (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Ooops, no results</h2>
+          </div>
+        )}
       </div>
     </main>
   )
